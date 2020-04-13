2f7a24fdc486a3c46e4890f00f101d98fbb77eb8
leomet07 <leomet07@gmail.com>
2020-04-12 20:36:25 -0400

    working pipe top collsion

--- main.py
+++ main.py
@@ -31,19 +31,49 @@ class Player(Rectangle):
         self.x_max_vel = x_max_vel
         self.y_max_vel = y_max_vel
 
-    def check_gravity_collide(self, rect):
-        if self.y + self.h >= rect.y and self.y + self.h <= rect.y + rect.h:
+    def check_gravity_collide(self, rect, pipe):
+        if (self.y + self.h >= rect.y and self.y + self.h <= rect.y + rect.h ) or (self.y + self.h >= pipe.y and self.y + self.h <= pipe.y + pipe.h and self.x + self.w >= pipe.x and self.x + self.w <= pipe.x + pipe.w):
             return True
         else:
             return False
 
-    def update(self, ground):
+    def update(self, ground, pipe):
         # if going to fall through, dont
-        if self.y + self.y_vel + self.h < ground.y:
-            self.y += self.y_vel
+
+        allow_y_vel = False
+        
+        #if legs are below pipe top and above pipe bottom
+        if (self.y + self.y_vel + self.h > pipe.y) and (self.y + self.y_vel + self.h < pipe.y + pipe.h)  and (self.x >= pipe.x and self.x <= pipe.x + pipe.w):
+            allow_y_vel = False
+            self.y += pipe.y - (self.y + self.h)
+            self.y_vel = 0
+
+        #ground collsion doesnt require x checking
+        if self.y + self.y_vel + self.h < ground.y :
+            allow_y_vel = True
         else:
             self.y += ground.y - (self.y + self.h)
             self.y_vel = 0
+            allow_y_vel = False
+
+
+        #pipe checking DOES require x checking
+        print("----")
+        #print(self.x > pipe.x and self.x < pipe.x + pipe.w)
+        print((self.y + self.y_vel + self.h < pipe.y))
+
+        #dont allow one round of gravity if it will make u get stuck in pipe
+        # stuck in pipe means head is below pipe and legs are in pipe
+
+
+        
+            
+        
+
+        #print(allow_y_vel)
+        if allow_y_vel:
+            self.y += self.y_vel
+
         self.x += self.x_vel
 
         # add friction
@@ -62,20 +92,27 @@ class Player(Rectangle):
 
 ground = Rectangle(0, 450, 500, 50, [0, 255, 0], ground_texture)
 player = Player(100, 50, 20, 30, [255, 0, 0], mario_texture, 0, 0, 5, 5)
+pipe = Rectangle(100, 390, 100, 20, [0, 255, 0], None)
+pipes = []
+
+
+for i in range(0,3):
+    pipeh = Rectangle(100, 390, 100, 20, [0, 255, 0], None)
+
 
 
 def update():
-    player.update(ground)
+    player.update(ground, pipe)
 
     # add one round of graviy to check if you will fall through
     amnt = 2
     # player.y += player.y_vel
 
-    if player.check_gravity_collide(ground):
+    if player.check_gravity_collide(ground, pipe):
 
         player.y_vel = 0
     else:
-        if player.y_vel + amnt < 10:
+        if player.y_vel + amnt < 15:
 
             player.y_vel = player.y_vel + amnt
 
@@ -91,6 +128,8 @@ def draw():
     # pygame.draw.rect(win, player.color, [player.x, player.y, player.w, player.h], 0)
     char = pygame.transform.scale(player.texture, (player.w, player.h))
     win.blit(char, (player.x, player.y))
+
+    pygame.draw.rect(win, pipe.color, [pipe.x, pipe.y, pipe.w, pipe.h], 0)
 
     pygame.display.update()
 
@@ -116,7 +155,8 @@ while run:
         if player.x_vel >= -4:
             player.x_vel += -4
     if keys[pygame.K_UP]:
-        player.y_vel = -10
+        if player.check_gravity_collide(ground, pipe):
+            player.y_vel = -20
     # run update after key recog
     update()
     draw()
